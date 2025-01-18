b66e4a0da61baca751b965a84845c44c32e4dfcf
dennamandela <denna.mandela13@gmail.com>
2025-01-18 20:10:52 +0700

    update app.js and Jenkinsfile

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className="App-description">
+          Tangerang adalah sebuah kota yang berada di Provinsi Banten, Indonesia. 
+          Kota ini dikenal dengan berbagai fasilitas modern, budaya yang beragam, 
+          dan infrastruktur yang berkembang pesat. Selamat menjelajahi Tangerang!
+        </p>
       </div>
     );
   }
